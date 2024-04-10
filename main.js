7c8add445d3f5e8692de6c8151b6f26acefafd65
silvaPool <silva.friends22@gmail.com>
2024-04-10 10:14:42 -0300

    Estudo VUE

---
+++ main.js
@@ -0,0 +1,15 @@
+let app = new Vue({
+
+    el: "#app",
+    data: {
+        title: "Matheus Silva Portfolio",
+        titleHTML: "Matheus Silva <span class='badge'>portfolio</span>",
+        projects: [
+            {title: "portfolio", languages: ["HTML", "CSS", "VueJS"]},
+            {title: "grocery shop", languages: ["HTML", "CSS", "PHP"]},
+            {title: "blog", languages: ["HTML", "CSS", "PHP"]},
+            {title: "automation script", languages: ["Python"]},
+            {title: "eCommerce", languages: ["HTML", "CSS", "PHP"]},
+        ]
+    }
+})
